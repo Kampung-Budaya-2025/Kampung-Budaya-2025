5e84c677f0890700609dcf4c7de065fd91942b05
AndreasBagasgoro <andreasbagasgoro04@gmail.com>
2025-08-17 21:56:12 +0700

    chore (routing) : Changing faq.blade.php to app.blade.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/{any}', function () {
-    return view('faq'); // atau ganti dengan view utama kamu, misal 'app'
+    return view('app'); // atau ganti dengan view utama kamu, misal 'app'
 })->where('any', '.*');
