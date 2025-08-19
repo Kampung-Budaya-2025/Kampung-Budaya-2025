b600abd14d4a672afdfb176505da2c4a3bb8ce26
Rizqi <rizqiaditya222@gmail.com>
2025-08-19 11:58:24 +0700

    feat(landing page): hero section, event,  mascot introduction

---
+++ resources/js/Components/LandingPage/FilosofiLogo/Page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FilosofiLogi = () => {
+  return (
+    <section className='relative h-screen'>
+                            <img className='w-full absolute top-32 transform z-30 rotate-180' src="img/background/batikan.svg" alt="" />
+
+    </section>
+  )
+}
+
+export default FilosofiLogi
