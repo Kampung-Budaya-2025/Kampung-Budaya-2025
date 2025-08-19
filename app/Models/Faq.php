84fc3feb850d94605488b1a78b3a92a553c85dc5
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2025-08-19 12:07:15 +0700

    feat(model): adding faq model

---
+++ app/Models/Faq.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Faq extends Model
+{
+    protected $fillable = [
+        'question',
+        'answer',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+}
