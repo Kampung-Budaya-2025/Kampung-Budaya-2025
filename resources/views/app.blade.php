6a09598f065648edb2f3a4a9f83ed29e79420596
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2025-09-04 20:04:00 +0700

    feat(app): adding favicon to app

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title inertia>{{ config('app.name', 'Kampung Budaya') }}</title>
+    
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     @if(app()->environment('local'))
         @viteReactRefresh
     @endif
